28fac652d7145f8ad46d349f494f3def87928a48
acedmicabhishek <acedmicabhishek@gmail.com>
2025-06-28 19:08:27 +0530

    IndexBuffer

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <cmath>
 #include "shader_utils.h"
 
 // Vertex shader source
@@ -46,25 +47,49 @@ int main() {
 
     // Triangle vertices
     float vertices[] = {
-         0.0f,  0.5f, 0.0f,  // Top
+        // Coordinates for a rectangle
+        0.5f,  0.5f, 0.0f,  // Top Right
+        0.5f, -0.5f, 0.0f,  // Bottom Right
         -0.5f, -0.5f, 0.0f,  // Bottom Left
-         0.5f, -0.5f, 0.0f   // Bottom Right
+        -0.5f,  0.5f, 0.0f   // Top Left
     };
 
-    GLuint VAO, VBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
+    GLuint indices[] =
+    {
+        0, 1, 3, // First Triangle
+        1, 2, 3  // Second Triangle
+    };
+
+    GLuint VAO, VBO, EBO; // unsigned int that is referenced to the GL
+    glGenVertexArrays(1, &VAO); // same as new keyword to create an empty obj
+    glGenBuffers(1, &VBO); // same thing
+    glGenBuffers(1, &EBO);
 
     // Bind VAO
-    glBindVertexArray(VAO);
+    glBindVertexArray(VAO); // Bind the VAO — from now on, all vertex attribute settings will be stored in it
 
     // Copy vertex data into VBO
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO); 
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     // Link VBO to vertex shader attribute 0
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+    // Bind the EBO specifying it's a GL_ELEMENT_ARRAY_BUFFER
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    // Introduce the indices into the EBO
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // Configure the Vertex Attribute so that OpenGL knows how to read the VBO
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    // Enable the Vertex Attribute so that OpenGL knows to use it
     glEnableVertexAttribArray(0);
+
+    // Bind both the VBO and VAO to 0 so that we don't accidentally modify the VAO and VBO we created
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+    // Bind the EBO to 0 so that we don't accidentally modify it
+    // NEED TO MAKE SURE TO UNBIND IT AFTER UNBINDING THE VAO, as the EBO is linked in the VAO
+    // This does not apply to the VBO because the VBO is already linked to the VAO during glVertexAttribPointer
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
     // Create shader program
     GLuint shaderProgram = createShaderProgram(vertexShaderSource, fragmentShaderSource);
@@ -76,7 +101,7 @@ int main() {
 
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -85,6 +110,7 @@ int main() {
     // Cleanup
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glDeleteProgram(shaderProgram);
 
     glfwDestroyWindow(window);
