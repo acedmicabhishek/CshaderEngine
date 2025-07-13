0d64bc4ec969987ee3eb06d6f22bfbcf844a8e68
acedmicabhishek <acedmicabhishek@gmail.com>
2025-07-13 08:04:52 +0530

    some fractal stuff

--- src/Shader.cpp
+++ src/Shader.cpp
@@ -57,6 +57,10 @@ void Shader::use() {
     glUseProgram(ID);
 }
 
+void Shader::Delete() {
+    glDeleteProgram(ID);
+}
+
 void Shader::setBool(const std::string &name, bool value) const {         
     glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value); 
 }
--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,8 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb/stb_image.h"
 
+#include "Shader.h"
+
 int main() {
     // Initialize GLFW
     if (!glfwInit()) {
@@ -46,62 +48,27 @@ int main() {
     // Triangle vertices with texture coordinates
     // Vertices coordinates
 
-    GLfloat lightVertices[] =
-    { //     COORDINATES     //
-        -0.1f, -0.1f,  0.1f,
-        -0.1f, -0.1f, -0.1f,
-         0.1f, -0.1f, -0.1f,
-         0.1f, -0.1f,  0.1f,
-        -0.1f,  0.1f,  0.1f,
-        -0.1f,  0.1f, -0.1f,
-         0.1f,  0.1f, -0.1f,
-         0.1f,  0.1f,  0.1f
+    GLfloat quadVertices[] = {
+        // Positions   // TexCoords
+        -1.0f,  1.0f,  0.0f, 1.0f,
+        -1.0f, -1.0f,  0.0f, 0.0f,
+         1.0f, -1.0f,  1.0f, 0.0f,
+
+        -1.0f,  1.0f,  0.0f, 1.0f,
+         1.0f, -1.0f,  1.0f, 0.0f,
+         1.0f,  1.0f,  1.0f, 1.0f
     };
 
-    GLuint lightIndices[] =
-    {
-        0, 1, 2,
-        0, 2, 3,
-        0, 4, 7,
-        0, 7, 3,
-        3, 7, 6,
-        3, 6, 2,
-        2, 6, 5,
-        2, 5, 1,
-        1, 5, 4,
-        1, 4, 0,
-        4, 5, 6,
-        4, 6, 7
-    };
+    // Setup quad VAO and VBO
+    VAO quadVAO;
+    quadVAO.Bind();
+    VBO quadVBO(quadVertices, sizeof(quadVertices));
+    quadVAO.LinkAttrib(quadVBO, 0, 2, GL_FLOAT, 4 * sizeof(float), (void*)0);
+    quadVAO.LinkAttrib(quadVBO, 1, 2, GL_FLOAT, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+    quadVAO.Unbind();
+    quadVBO.Unbind();
 
-
-
-    // Shader for light cube
-    Shader lightShader("../shaders/light.vert", "../shaders/light.frag");
-    // Generates Vertex Array Object and binds it
-    VAO lightVAO;
-    lightVAO.Bind();
-    // Generates Vertex Buffer Object and links it to vertices
-    VBO lightVBO(lightVertices, sizeof(lightVertices));
-    // Generates Element Buffer Object and links it to indices
-    EBO lightEBO(lightIndices, sizeof(lightIndices));
-    // Links VBO attributes such as coordinates and colors to VAO
-    lightVAO.LinkAttrib(lightVBO, 0, 3, GL_FLOAT, 3 * sizeof(float), (void*)0);
-    // Unbind all to prevent accidentally modifying them
-    lightVAO.Unbind();
-    lightVBO.Unbind();
-    lightEBO.Unbind();
-
-
-
-    glm::vec4 lightColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
-    glm::vec3 lightPos = glm::vec3(0.5f, 0.5f, 0.5f);
-    glm::mat4 lightModel = glm::mat4(1.0f);
-    lightModel = glm::translate(lightModel, lightPos);
-
-    lightShader.use();
-    glUniformMatrix4fv(glGetUniformLocation(lightShader.ID, "model"), 1, GL_FALSE, glm::value_ptr(lightModel));
-    glUniform4f(glGetUniformLocation(lightShader.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
+    Shader fractalShader("../shaders/fractal.vert", "../shaders/fractal.frag");
 
     // Enables the Depth Buffer
     glEnable(GL_DEPTH_TEST);
@@ -119,18 +86,15 @@ int main() {
 
         // Handles camera inputs
         camera.Inputs(window);
-        // Updates and exports the camera matrix to the Vertex Shader
 
+        fractalShader.use();
+        glUniform2f(glGetUniformLocation(fractalShader.ID, "iResolution"), 800.0f, 600.0f);
+        glUniform1f(glGetUniformLocation(fractalShader.ID, "iTime"), glfwGetTime());
+        glUniform3f(glGetUniformLocation(fractalShader.ID, "camPos"), camera.Position.x, camera.Position.y, camera.Position.z);
+        camera.Matrix(45.0f, 0.1f, 100.0f, fractalShader, "camMatrix");
 
-        // Tells OpenGL which Shader Program we want to use
-        lightShader.use();
-        // Export the camMatrix to the Vertex Shader of the light cube
-        camera.Matrix(45.0f, 0.1f, 100.0f, lightShader, "camMatrix");
-        // Bind the VAO so OpenGL knows to use it
-        lightVAO.Bind();
-        // Draw primitives, number of indices, datatype of indices, index of indices
-        glDrawElements(GL_TRIANGLES, sizeof(lightIndices) / sizeof(int), GL_UNSIGNED_INT, 0);
-
+        quadVAO.Bind();
+        glDrawArrays(GL_TRIANGLES, 0, 6);
 
         // Swap the back buffer with the front buffer
         glfwSwapBuffers(window);
@@ -138,14 +102,10 @@ int main() {
         glfwPollEvents();
     }
 
-
-
     // Delete all the objects we've created
-    lightVAO.Delete();
-    lightVBO.Delete();
-    lightEBO.Delete();
-    //lightShader.Delete();
-    // Delete window before ending the program
+    quadVAO.Delete();
+    quadVBO.Delete();
+    fractalShader.Delete();
 
     glfwDestroyWindow(window);
     glfwTerminate();
